2689fdf3ad97e1e31c0875f6b907aa003948ac7d
IkramGhalib <39878945+IkramGhalib@users.noreply.github.com>
2020-10-16 22:12:48 +0500

    build create

---
+++ build/precache-manifest.e6f93fed63b353a3eb86717fcd1607f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0742e8a8866e495e5c877d886fba416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f9cf5778fd89a025a2f",
+    "url": "/static/css/main.8c7c6cba.chunk.css"
+  },
+  {
+    "revision": "17da47e82e032e0eebb7",
+    "url": "/static/js/2.c45172e8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c45172e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9cf5778fd89a025a2f",
+    "url": "/static/js/main.12098594.chunk.js"
+  },
+  {
+    "revision": "2fff04eb8d07d01e602d",
+    "url": "/static/js/runtime-main.e52e0b8f.js"
+  }
+]);
